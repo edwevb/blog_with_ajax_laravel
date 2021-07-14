3f41dcf01cc363d5987bf4fc41520d752ef84c97
edward evbert <edwardevbert@gmail.com>
2021-07-15 02:16:25 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     /*
@@ -27,6 +27,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function authenticated(Request $request, $user)
+    {
+        return redirect('/admin/dashboard');
+    }
+
 
     protected function loggedOut()
     {
