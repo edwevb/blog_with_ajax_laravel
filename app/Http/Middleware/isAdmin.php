c9e8608f7e7fdae6ec34805f85285c4bc2e16ccd
edward evbert <edwardevbert@gmail.com>
2021-07-15 00:52:25 +0700

    update

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -6,16 +6,9 @@ use Closure;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->roles){
+        if(auth()->user()->roles == 1){
             return $next($request);
         }
         abort(403, 'Access Forbidden!');
